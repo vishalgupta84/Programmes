34bc5114d94a876d6a47af559e64addb013e7510
vishalgupta84 <gupta.vishalarya@gmail.com>
2016-08-11 13:12:58 +0530

    solved more

---
+++ coding/geeksforgeeks/mirror.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int key;
+	struct Node *left,*right;
+};
+
+typedef struct Node node;
+
+
+
+int main(){
+	int n;
+	cin >> n;	
+	return 0;
+}
