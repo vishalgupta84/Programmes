4a1d075c3dde38fc26c48fa33999d24f37f78690
vishalgupta84 <gupta.vishalarya@gmail.com>
2016-08-12 19:28:03 +0530

    solve que of stack & queue from interviewbit

---
+++ coding/interviewbit/stack & queue/RainWater.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void print(vector<int> A){
+	for(int i=0;i<A.size();i++)
+		cout << A[i] <<" ";
+	cout << endl;
+}
+int max(int a,int b){
+	return ((a>b)?a:b);
+}
+int min(int a,int b){
+	return ((a<b)?a:b);
+}
+int evalute(vector<int> A){
+	int sum=0;
+	vector<int> left(A.size());
+	vector<int> right(A.size());
+	left[0]=A[0];
+	right[A.size()-1]=A[A.size()-1];
+	for(int i=1;i<A.size();i++)
+		left[i]=max(left[i-1],A[i]);
+	for(int i=A.size()-2;i>=0;i--)
+		right[i]=max(right[i+1],A[i]);
+	for(int i=0;i<A.size();i++)
+		sum=sum+min(left[i],right[i])-A[i];
+	//print(A);
+	//print(left);
+	//print(right);
+	return sum;
+}
+int main(){
+	int n;
+	cin >> n;	
+	vector<int> A(n);
+	for(int i=0;i<n;i++)
+		cin >> A[i];
+	int res=evalute(A);
+	cout << res;
+	return 0;
+}
